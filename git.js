5ffcedc8285631d16c3d274982a430f62fc98a38
Niiijima <qagwu11@gmail.com>
2026-04-08 19:19:03 +0100

    week 4

--- git.js
+++ git.js
@@ -1,2 +1,11 @@
 //Git Commands
 //assignment: write a 3-paged report on version control systems, types, advantages and differences between the types(images and commands are allowed).
+// to link your github account do the following: git config--global user.name "Your user name" and git config--global user.email"your email address"
+//to know if your git was configured (git config--global--list)
+
+//basic git commands:
+
+//create a git repository use git init
+//to clone a repository use git clone
+//to add changes from your working directory to the staging area use git add
+//to record changes in the staging area to the repository use git commit
