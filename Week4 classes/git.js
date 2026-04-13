e0b7b4ac3b58bdbc41e0d61ddabb3a3aebe11179
Niiijima <qagwu11@gmail.com>
2026-04-13 20:20:52 +0100

    edit

--- Week4 classes/git.js
+++ Week4 classes/git.js
@@ -12,3 +12,13 @@
 //to create a new branch use git branch
 //to change branches use git branch -M (name of branch)
 //this links your repository with your working copy on your laptop git remote add origin
+//to view the current state of things use the git status
+//to dispay the commit history, commit ID and commit messages and author's information use git log(it is useful for reviewing cahnges and tracking progress)
+//master or main are both default branches that github gives you
+//to push your code to your git hub use git push
+//to fetch changes from your github(remote repo) to  your laptop, allowing you to merge in the local repository(laptop) use git pull
+//working directory is the dirctory where you modify files
+//staging area is also known as the index, it holds changes that are ready to be committed. files are added here through the use of git add
+//the git directed is the repository itself, containing all git internal structures, including the history of all the changes youve made
+//untracked files(u) the files are new and not yet tracked by git
+//tracked (modified, staged and commited) when the files has been tracked by git
